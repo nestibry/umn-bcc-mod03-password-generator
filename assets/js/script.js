8154d35c9c622dabfcfde1b86368b33447380c7a
nestibry <nestibry@gmail.com>
2023-09-29 15:57:41 -0500

    added an abort prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,34 @@ var generateBtn = document.querySelector("#generate");
 /* ------------------------------------------------------------------- */
 
 // Global variables
+var newPassword;
 var numChar;  // Password Length
 
 
 
 function userPrompts() {
     numChar = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
-    if (numChar == null){
-        numChar = "numChar is null";
-        return;
+
+    console.log(numChar + " " + typeof numChar);
+
+    if (numChar === null){
+        do {
+            var confirmAbort = confirm("Exit Password Generator?");
+            if (confirmAbort){
+                return numChar = "Joe Six-pack";
+            }
+            numChar = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
+        }
+        while(numChar === null);
     }
+
+
+
+    // if (numChar == null){
+    //     numChar = "numChar is null";
+    //     return;
+    // }
+    //do
     console.log(numChar);
 
 }
