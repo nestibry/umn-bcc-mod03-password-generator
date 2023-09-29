ce813cd4e581a3f3ffa6110a77b207b5588e91a9
nestibry <nestibry@gmail.com>
2023-09-29 16:27:30 -0500

    changed all the numChar variables to passwordLength

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,46 +7,49 @@ var generateBtn = document.querySelector("#generate");
 
 // Global variables
 var newPassword;
-var numChar;  // Password Length
+var passwordLength;  // Password Length
 
 
 
 // function userPrompts() {
 // }
 
+function lengthPrompt() {
+    
+}
 
 
 
 function generatePassword() {
 
     // Choose Password Length
-    numChar = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
-    console.log(`User chose password length: ${numChar}  type: ${typeof numChar}`);
+    passwordLength = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
+    console.log(`User chose password length: ${passwordLength}  type: ${typeof passwordLength}`);
 
     // Case: User clicks cancel
-    if (numChar === null){
+    if (passwordLength === null){
         do {
             var confirmAbort = confirm("Exit Password Generator?");
             if (confirmAbort){
                 // Aborts to default screen with "Your Secure Password"
                 return null;
             }
-            numChar = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
-            console.log(`User chose password length: ${numChar}  type: ${typeof numChar}`);
+            passwordLength = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
+            console.log(`User chose password length: ${passwordLength}  type: ${typeof passwordLength}`);
         }
-        while(numChar === null);
+        while(passwordLength === null);
     }
 
     // Case: Check that the value is a number
-    numChar *= 1; //converts to a number
-    console.log(`Converted to: ${numChar}  type: ${typeof numChar}`);
-    if(!numChar){
+    passwordLength *= 1; //converts to a number
+    console.log(`Converted to: ${passwordLength}  type: ${typeof passwordLength}`);
+    if(!passwordLength){
         console.log("user did not enter a number")
         return null;
     }
 
 
-    return numChar;
+    return passwordLength;
 }
 
 
