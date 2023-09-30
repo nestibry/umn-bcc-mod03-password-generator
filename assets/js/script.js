e4c3c98178b5b4196d97c845e6bdaf36885fdc45
nestibry <nestibry@gmail.com>
2023-09-30 09:23:48 -0500

    finalized the lenghtPrompt(). need to add in something later in userPrompts() or generatePassword() for the condition  if the passwordLength is null that the generation process needs to be aborted

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,30 +18,34 @@ function lengthPrompt() {
     var numChar = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
     console.log(`Password Length: ${numChar}  Type: ${typeof numChar}`);
 
-    // Case: User clicks cancel
+    // Case: User clicks cancel --> User can either click 'OK' to exit password generator OR 'Cancel' to re-prompt the lengthPrompt()
     if (numChar === null){
         do {
             var confirmAbort = confirm("Exit Password Generator?");
             if (confirmAbort){
-                // Aborts to default screen with "Your Secure Password"
+                // Aborts to default screen with "Your Secure Password" --> returns null for the 'numChar'
                 console.log("User selected to abort Password Generator...")
                 return null;
             }
-            numChar = prompt(`Password Length: Choose a length of at least 8 characters and no more than 128 characters...`);
-            console.log(`Password Length: ${numChar}  Type: ${typeof numChar}`);
+            numChar = lengthPrompt();
         }
         while(numChar === null);
     }
 
-    // Case: Check that the value is a number
+    // Case: Check that the value is a number AND within range [8:128] -- Re-prompt user to enter password length
     numChar *= 1;   // converts to a number
     console.log(`Converted to: ${numChar}  type: ${typeof numChar}`);
+
     if(!numChar){
-        // Aborts to default screen with "Your Secure Password"
-        console.log("User did not enter a number. Aborting Password Generator...")
-        return null;
-    } else if (numChar < 8 || numChar) {
         
+        console.log("User did not enter a number. Re-prompting user to enter password length...")
+        numChar = lengthPrompt();
+
+    } else if (numChar < 8 || numChar > 128) {
+        
+        console.log("User did not enter a number between 8 and 128. Re-prompting user to enter password length...")
+        numChar = lengthPrompt();
+
     }
 
     return numChar;
